f267058f3a1c401de0d0065e1932e87f1a7fe64e
Christopher Allen <93797239+ce-ALLEN@users.noreply.github.com>
2022-02-27 14:29:00 -0700

    added .env

--- server.js
+++ server.js
@@ -73,6 +73,15 @@ router.post('/signin', function (req, res) {
 });
 
 router.route('/movies')
+    .get(function (req, res) {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        var o = getJSONObjectForMovieRequirement(req);
+        res.json(o)
+    })
     .delete(authController.isAuthenticated, function(req, res) {
         console.log(req.body);
         res = res.status(200);
